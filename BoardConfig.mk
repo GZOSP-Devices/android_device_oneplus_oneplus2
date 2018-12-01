d1a41099e138719fefcab4a1e09b46b47f292530
hardwareadictos <hardwareadictos@gmail.com>
2018-12-01 13:07:58 +0100

    oneplus2: ignore sepolicy NEVERALLOWS

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,6 +172,7 @@ TARGET_USES_INTERACTION_BOOST := true
 BOARD_USES_QCOM_HARDWARE := true
 
 # SELinux
+SELINUX_IGNORE_NEVERALLOWS := true
 #include device/qcom/sepolicy-legacy/sepolicy.mk
 #BOARD_SEPOLICY_DIRS += $(PLATFORM_PATH)/sepolicy
 
